ac7e3151e8b42dd91302bac9cdc96a0d3d593b70
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-16 21:36:48 +0500

    GroupTypeConfiguration

---
+++ MaxEdu.Application/Configuration/Groups/GroupTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using MaxEdu.Domain.Entities.Groups;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MaxEdu.Application.Configuration.Groups
+{
+    partial class GroupTypeConfiguration : IEntityTypeConfiguration<Group>
+    {
+        public void Configure(EntityTypeBuilder<Group> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.GroupName)
+                .IsRequired();
+        }
+    }
+}
