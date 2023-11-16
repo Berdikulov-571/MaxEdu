1f90b5515c583f52d6ed2e9e44a00db611183fce
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-16 21:36:59 +0500

    LessonTypeConfiguration

---
+++ MaxEdu.Application/Configuration/Lessons/LessonTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using MaxEdu.Domain.Entities.Lessons;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MaxEdu.Application.Configuration.Lessons
+{
+    public class LessonTypeConfiguration : IEntityTypeConfiguration<Lesson>
+    {
+        public void Configure(EntityTypeBuilder<Lesson> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.LessonTheme)
+                .IsRequired();
+
+            builder.Property(x => x.StartedDateTime).IsRequired(true);
+
+            builder.HasOne(x => x.Group)
+                .WithMany(x => x.Lessons);
+
+            builder.HasOne(x => x.Group)
+                .WithMany(x => x.Lessons);
+        }
+    }
+}
