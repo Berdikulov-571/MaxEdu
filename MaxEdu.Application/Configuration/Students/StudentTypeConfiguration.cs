70828b225c43f8eb47dd03bf5ac84269ba9a45e0
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-16 21:37:14 +0500

    StudentTypeConfiguration

---
+++ MaxEdu.Application/Configuration/Students/StudentTypeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using MaxEdu.Domain.Entities.Groups;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MaxEdu.Application.Configuration.Students
+{
+    public class StudentTypeConfiguration : IEntityTypeConfiguration<StudentGroup>
+    {
+        public void Configure(EntityTypeBuilder<StudentGroup> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.HasOne(x => x.User)
+                .WithMany(x => x.StudentGroups);
+
+            builder.HasOne(x => x.Group)
+                .WithMany(x => x.StudentGroups)
+                .HasForeignKey(x => x.GroupId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
