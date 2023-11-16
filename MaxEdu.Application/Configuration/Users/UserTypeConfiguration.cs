40ba034c09ea3185f80ffd544a5c4b6f4c090c17
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-16 21:37:27 +0500

    UserTypeConfiguration

---
+++ MaxEdu.Application/Configuration/Users/UserTypeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using MaxEdu.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MaxEdu.Application.Configuration.Users
+{
+    public class UserTypeConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.UserName)
+                .IsRequired();
+
+            builder.HasMany(x => x.StudentGroups)
+                .WithOne(x => x.User);
+
+            builder.HasMany(x => x.Groups)
+                .WithOne(x => x.User);
+
+            builder.HasMany(x => x.Attendances)
+                .WithOne(x => x.User);
+        }
+    }
+}
